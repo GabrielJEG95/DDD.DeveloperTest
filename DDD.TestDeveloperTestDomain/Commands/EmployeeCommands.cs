dba8054f46d593de95e5d3d58845dcf49abfe2a5
GabrielJEG95 <gabrieljeg95@gmail.com>
2022-09-03 23:50:12 -0600

    validacion de registro de empleado

---
+++ DDD.TestDeveloperTestDomain/Commands/EmployeeCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace DDD.TestDeveloperTestDomain.Commands
+{
+    public abstract class EmployeeCommands : Command
+    {
+        public int Id { get; protected set; }
+        public int? Year { get; protected set; }
+        public int? Month { get; protected set; }
+        public int? Office { get; protected set; }
+        public string? EmployeeCode { get; protected set; }
+        public string? EmployeeName { get; protected set; }
+        public string? EmployeeSurname { get; protected set; }
+        public int? Division { get; protected set; }
+        public int? Position { get; protected set; }
+        public int? Grade { get; protected set; }
+        public DateTime? BeginDate { get; protected set; }
+        public DateTime? Birthday { get; protected set; }
+        public string? IdentificationNumber { get; protected set; }
+        public decimal? BaseSalary { get; protected set; }
+        public decimal? ProductionBonus { get; protected set; }
+        public decimal? CompensationBonus { get; protected set; }
+        public decimal? Commision { get; protected set; }
+        public decimal? Contributions { get; protected set; }
+    }
+}
--- DDD.TestDeveloperTestDomain/Validaciones/EmployeeValidation.cs
+++ DDD.TestDeveloperTestDomain/Validaciones/EmployeeValidation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DDD.TestDeveloperTestDomain.Commands;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace DDD.TestDeveloperTestDomain.Validaciones
 {
-    internal class EmployeeValidation
+    public abstract class EmployeeValidation<T> : AbstractValidator<T> where T : EmployeeCommands
     {
+        protected void validateName()
+        {
+            RuleFor(c => c.EmployeeName)
+                .NotEmpty().WithMessage("Favor ingresar el nombre del empleado")
+                .Length(2, 150).WithMessage("El nombre debe tener entre 2 y 150 caracteres");
+        }
+        protected void validateSurname()
+        {
+            RuleFor(c => c.EmployeeSurname)
+                .NotEmpty().WithMessage("Favor ingresar el apellido del empleado")
+                .Length(2, 150).WithMessage("El apellido debe tener entre 2 y 150 caracteres");
+        }
+        protected void ValidateBithDay()
+        {
+            _ = RuleFor(c => c.Birthday)
+                .NotEmpty()
+                .Must(EdadMinima)
+                .WithMessage("El empleado debe ser mayor de edad");
+        }
+
+        protected static bool EdadMinima(DateTime bithday)
+        {
+            return bithday <= DateTime.Now.AddYears(-18);
+        }
     }
 }
